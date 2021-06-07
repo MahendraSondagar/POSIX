3907666ba0e7e99d300d3ad5ffc710a81d7f1513
mahendra_sondagar <mahendrasondagar08@gmail.com>
2021-06-07 23:59:34 +0530

    signal

--- signal_01.c
+++ signal_01.c
@@ -13,6 +13,8 @@ int main(void)
     /* Creating the signal & attaching the handler*/
     /* Handler will call, when user press ctl+c */
     signal(SIGINT, MyHandler);
+    /* Handler will call, when try to kill the process :)*/
+    signal(SIGTERM, MyHandler);
     while(1)
     {
         printf("Running process: %d\r\n", getpid());
